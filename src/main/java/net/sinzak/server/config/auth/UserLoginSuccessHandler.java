7143e5f4c6028725e1f183ed8a436da2acf5767a
starwook <star.wookk@gmail.com>
2022-10-29 02:51:24 +0900

    add thymeleaf templates

--- src/main/java/net/sinzak/server/config/auth/UserLoginSuccessHandler.java
+++ src/main/java/net/sinzak/server/config/auth/UserLoginSuccessHandler.java
@@ -13,6 +13,6 @@ public class UserLoginSuccessHandler implements AuthenticationSuccessHandler {
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
-        response.sendRedirect("http://localhost:8080/home.html");
+        response.sendRedirect("http://localhost:8080/home");
     }
 }
