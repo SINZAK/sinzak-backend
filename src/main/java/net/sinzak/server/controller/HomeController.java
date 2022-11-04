ca9aedfd53a91368a3c63c6b976b4bbf2c87e7ed
in-seo <sis000512@gmail.com>
2022-11-04 21:13:26 +0900

    feat: add thymeleaf form

--- src/main/java/net/sinzak/server/controller/HomeController.java
+++ src/main/java/net/sinzak/server/controller/HomeController.java
@@ -1,15 +1,22 @@
 package net.sinzak.server.controller;
 
 import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import net.sinzak.server.domain.User;
+import net.sinzak.server.repository.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final UserRepository userRepository;
 
     @ApiOperation(value = "API 명세서")
     @RequestMapping("/api")
@@ -18,6 +25,13 @@ public class HomeController {
     @GetMapping("home")
     public String hello(Model model){
         return "home";
+    }
+
+    @GetMapping("/userList")
+    public String userList(Model model){
+        List<User> users = userRepository.findAll();
+        model.addAttribute("users",users);
+        return "userList";
     }
 
 }
