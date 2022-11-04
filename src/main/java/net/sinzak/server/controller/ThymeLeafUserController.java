bcb8a7ebe371ae7342cdff75cb8fbae36f71a845
starwook <star.wookk@gmail.com>
2022-11-05 00:06:40 +0900

    refactor thymeleaf

--- src/main/java/net/sinzak/server/controller/ThymeLeafUserController.java
+++ src/main/java/net/sinzak/server/controller/ThymeLeafUserController.java
@@ -1,12 +1,15 @@
 package net.sinzak.server.controller;
 
 import lombok.RequiredArgsConstructor;
+import net.sinzak.server.domain.User;
 import net.sinzak.server.repository.UserRepository;
 import net.sinzak.server.service.UserCommandService;
 import net.sinzak.server.service.UserQueryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,7 +20,8 @@ public class ThymeLeafUserController {
     @RequestMapping(value ="/home")
     public ModelAndView home(){
         ModelAndView modelAndView = new ModelAndView("home");
-        modelAndView.addObject("userList", userRepository.findAll());
+        List<User> users = userRepository.findAll();
+        modelAndView.addObject("userList", users);
         return modelAndView;
     }
 
