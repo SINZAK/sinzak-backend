2b3e70b488852e41f07d014a407a8bfd7f3434e2
in-seo <sis000512@gmail.com>
2022-11-04 21:44:46 +0900

    feat: add view productList, workList with thymeleaf

--- src/main/java/net/sinzak/server/controller/HomeController.java
+++ src/main/java/net/sinzak/server/controller/HomeController.java
@@ -2,8 +2,12 @@ package net.sinzak.server.controller;
 
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import net.sinzak.server.domain.Product;
 import net.sinzak.server.domain.User;
+import net.sinzak.server.domain.Work;
+import net.sinzak.server.repository.ProductRepository;
 import net.sinzak.server.repository.UserRepository;
+import net.sinzak.server.repository.WorkRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +21,8 @@ import java.util.List;
 public class HomeController {
 
     private final UserRepository userRepository;
+    private final ProductRepository productRepository;
+    private final WorkRepository workRepository;
 
     @ApiOperation(value = "API 명세서")
     @RequestMapping("/api")
@@ -32,6 +38,20 @@ public class HomeController {
         List<User> users = userRepository.findAll();
         model.addAttribute("users",users);
         return "userList";
+    }
+
+    @GetMapping("/productList")
+    public String productList(Model model){
+        List<Product> products = productRepository.findAll();
+        model.addAttribute("products",products);
+        return "productList";
+    }
+
+    @GetMapping("/workList")
+    public String workList(Model model){
+        List<Work> works = workRepository.findAll();
+        model.addAttribute("works",works);
+        return "workList";
     }
 
 }
