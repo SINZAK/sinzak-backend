d1a309c714e3428161bfb564a9680ae520638d09
in-seo <sis000512@gmail.com>
2022-11-04 21:55:27 +0900

    feat: add view productList, workList with thymeleaf(2)

--- src/main/java/net/sinzak/server/controller/ThymeLeafUserController.java
+++ src/main/java/net/sinzak/server/controller/ThymeLeafUserController.java
@@ -1,10 +1,14 @@
 package net.sinzak.server.controller;
 
 import lombok.RequiredArgsConstructor;
+import net.sinzak.server.repository.ProductRepository;
 import net.sinzak.server.repository.UserRepository;
+import net.sinzak.server.repository.WorkRepository;
 import net.sinzak.server.service.UserCommandService;
 import net.sinzak.server.service.UserQueryService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -14,11 +18,31 @@ public class ThymeLeafUserController {
     private final UserCommandService userCommandService;
     private final UserQueryService userQueryService;
     private final UserRepository userRepository;
+    private final WorkRepository workRepository;
+    private final ProductRepository productRepository;
     @RequestMapping(value ="/home")
     public ModelAndView home(){
         ModelAndView modelAndView = new ModelAndView("home");
         modelAndView.addObject("userList", userRepository.findAll());
         return modelAndView;
+    }
+
+    @GetMapping("/userList")
+    public String showUserList(Model model){
+        model.addAttribute("users",userRepository.findAll());
+        return "userList";
+    }
+
+    @GetMapping("/productList")
+    public String showProductList(Model model){
+        model.addAttribute("products",productRepository.findAll());
+        return "productList";
+    }
+
+    @GetMapping("/workList")
+    public String showWorkList(Model model){
+        model.addAttribute("works",workRepository.findAll());
+        return "workList";
     }
 
 }
