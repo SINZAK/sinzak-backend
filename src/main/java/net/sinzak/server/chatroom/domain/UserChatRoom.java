424496dc4ad146e819c83e7bb41ed60ae6b7e4a6
starwook <star.wookk@gmail.com>
2022-12-07 03:11:40 +0900

    feat: add UserChatRoom domain

--- src/main/java/net/sinzak/server/chatroom/domain/UserChatRoom.java
+++ src/main/java/net/sinzak/server/chatroom/domain/UserChatRoom.java
@@ -1,5 +1,28 @@
 package net.sinzak.server.chatroom.domain;
 
+
+import lombok.Getter;
+import net.sinzak.server.user.domain.User;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
 public class UserChatRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="CHATROOM_ID")
+    private ChatRoom chatRoom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="USER_ID")
+    private User user;
+
+    private String roomName;
+
 
 }
