14ccbae2fa27d5f5d16d73bf89df2889c9eeb2a5
starwook <star.wookk@gmail.com>
2022-12-07 02:28:45 +0900

    feat: add chatMessageDto

--- src/main/java/net/sinzak/server/chatroom/controller/ChatMessageController.java
+++ src/main/java/net/sinzak/server/chatroom/controller/ChatMessageController.java
@@ -1,14 +1,13 @@
 package net.sinzak.server.chatroom.controller;
 
 
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import net.sinzak.server.chatroom.domain.ChatMessage;
+import net.sinzak.server.chatroom.dto.ChatMessageDto;
 import net.sinzak.server.chatroom.service.ChatMessageService;
 import org.json.simple.JSONObject;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,9 +20,10 @@ public class ChatMessageController {
         return chatMessageService.getChatMessage(id);
     }
 
-    @GetMapping("/createChatMessage")
-    public JSONObject jsonObject(@RequestParam String message) throws Exception{
-        return chatMessageService.createChatMessage(message);
+    @ApiOperation(value = "채팅 메시지 생성")
+    @PostMapping("/createChatMessage")
+    public JSONObject jsonObject(@RequestBody ChatMessageDto chatMessageDto) throws Exception{
+        return chatMessageService.createChatMessage(chatMessageDto);
     }
 
 }
--- src/main/java/net/sinzak/server/chatroom/service/ChatMessageService.java
+++ src/main/java/net/sinzak/server/chatroom/service/ChatMessageService.java
@@ -7,26 +7,34 @@ import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.cloud.FirestoreClient;
 import net.sinzak.server.chatroom.domain.ChatMessage;
+import net.sinzak.server.chatroom.dto.ChatMessageDto;
 import net.sinzak.server.common.PropertyUtil;
 import org.json.simple.JSONObject;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class ChatMessageService {
 
     public static final String COLLECTION_NAME="chatMessage";
 
-    public JSONObject createChatMessage(String message) throws Exception{
-        ChatMessage chatMessage = new ChatMessage();
-        chatMessage.setId("1");
-        chatMessage.setMessage(message);
-        chatMessage.setSender("송인서");
-        chatMessage.setReceiver("유성욱");
-        chatMessage.setType("test");
+    public JSONObject createChatMessage(ChatMessageDto chatMessageDto) throws Exception{
+        ChatMessage chatMessage = makeTestMessage(chatMessageDto);
         Firestore firestore = FirestoreClient.getFirestore();
         ApiFuture<WriteResult> apiFuture =
                 firestore.collection(COLLECTION_NAME).document(chatMessage.getId()).set(chatMessage);
         return PropertyUtil.response(true);
+    }
+
+    private ChatMessage makeTestMessage(ChatMessageDto chatMessageDto) {
+        ChatMessage chatMessage = new ChatMessage();
+        chatMessage.setId(UUID.randomUUID().toString());
+        chatMessage.setMessage(chatMessageDto.getMessage());
+        chatMessage.setSender(chatMessageDto.getSender());
+        chatMessage.setReceiver("유성욱");
+        chatMessage.setType("test");
+        return chatMessage;
     }
 
     public ChatMessage getChatMessage(String id) throws Exception{
