210f1c5caf6f8c606859fac088ff13c3e595cf38
in-seo <sis000512@gmail.com>
2022-12-20 17:47:42 +0900

    feat: scp-action CI/CD service (6) - complete redirect URL

--- src/main/java/net/sinzak/server/config/auth/UserLoginSuccessHandler.java
+++ src/main/java/net/sinzak/server/config/auth/UserLoginSuccessHandler.java
@@ -13,6 +13,6 @@ public class UserLoginSuccessHandler implements AuthenticationSuccessHandler {
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
-        response.sendRedirect("/");
+        response.sendRedirect("/home");
     }
 }
