cdc6d8e54c50b262e4caffc47ed9b83ce726773e
starwook <star.wookk@gmail.com>
2022-12-29 01:25:41 +0900

    refactor: getUserProfile 변경 (세션->토큰)

--- src/main/java/net/sinzak/server/user/service/UserQueryService.java
+++ src/main/java/net/sinzak/server/user/service/UserQueryService.java
@@ -5,12 +5,12 @@ import net.sinzak.server.common.PropertyUtil;
 import net.sinzak.server.config.auth.dto.SessionUser;
 import net.sinzak.server.user.dto.respond.GetFollowDto;
 import net.sinzak.server.user.domain.User;
+import net.sinzak.server.user.dto.respond.UserDto;
 import net.sinzak.server.user.repository.UserRepository;
-import org.hibernate.metamodel.model.convert.spi.JpaAttributeConverter;
 import org.json.simple.JSONObject;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 
+import javax.management.InstanceNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -30,24 +30,35 @@ public class UserQueryService {
 //        WorkPostDto dto = new WorkPostDto("테스트","내용테스트");
 //    }
 
-    public JSONObject getUserProfile(Long otherUserId, SessionUser user) {
-        Optional<User> User = userRepository.findByEmail(user.getEmail());
+    public UserDto getUserProfile(Long otherUserId, User user) {
         Optional<User> findUser = userRepository.findById(otherUserId);
-        return checkIfTwoUserPresent(User,findUser);
+        return checkIfTwoUserPresent(user,findUser);
     }
-    private JSONObject makeJson(Optional<User> User, Optional<User> findUser) {
-        JSONObject object = new JSONObject();
-        refreshFollowNumber(findUser);
-        putUserInformation(object, findUser);
-        object.put("myProfile",checkIfMyProfile(User, findUser));
-        return object;
+    public UserDto checkIfTwoUserPresent(User user,Optional<User> findUser){
+        if(!findUser.isPresent()){
+            throw new IllegalArgumentException("찾는 유저가 존재하지 않음");
+        }
+        return makeUserDto(user,findUser);
     }
-    public void putUserInformation(JSONObject object, Optional<User> findUser) {
-        User user = findUser.get();
-        object.put("name", user.getName());
-        object.put("introduction", user.getIntroduction());
-        object.put("followingNumber", user.getFollowingNum());
-        object.put("followerNumber", user.getFollowerNum());
+    private UserDto makeUserDto(User user, Optional<User> findUser) {
+        refreshFollowNumber(findUser); //팔로우,팔로잉 숫자-> 한글
+        UserDto userDto = UserDto.builder()
+                .name(findUser.get().getName())
+                .introduction(findUser.get().getIntroduction())
+                .followingNumber(findUser.get().getFollowingNum())
+                .followerNumber(findUser.get().getFollowerNum())
+                .myProfile(checkIfMyProfile(user,findUser))
+                .build();
+        return userDto;
+    }
+    public boolean checkIfMyProfile(User user, Optional<User> findUser){
+        if(user == null){
+            return false;
+        }
+        if(findUser.get().getId().equals(user.getId())){
+            return true;
+        }
+        return false;
     }
     //팔로워가져오기
     public List<GetFollowDto> getFollowerDtoList(Long userId){
@@ -107,20 +118,7 @@ public class UserQueryService {
         }
         return "";
     }
-    public boolean checkIfMyProfile(Optional<User> User, Optional<User> findUser){
-        if(findUser.get().equals(User.get())){
-            return true;
-        }
-        return false;
-    }
-    public JSONObject checkIfTwoUserPresent(Optional<User> User,Optional<User> findUser){
-        if(!User.isPresent()){
-            return PropertyUtil.responseMessage("로그인한 유저가 존재하지 않음");
-        }
-        if(!findUser.isPresent()){
-            return PropertyUtil.responseMessage("찾는 유저가 존재하지 않음");
-        }
-        return makeJson(User,findUser);
-    }
+
+
 
 }
