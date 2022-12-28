fbfd9c87dce21b4bfbc2f98d07941c274d731338
starwook <star.wookk@gmail.com>
2022-12-29 01:24:55 +0900

    feat: UserDto.java 추가

---
+++ src/main/java/net/sinzak/server/user/dto/respond/UserDto.java
@@ -0,0 +1,15 @@
+package net.sinzak.server.user.dto.respond;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+public class UserDto {
+    private boolean myProfile;
+    private String name;
+    private String introduction;
+    private String followingNumber;
+    private String followerNumber;
+}
