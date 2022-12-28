bda397f203ab3dd2b81e2f8c2a74519138f76c8a
starwook <star.wookk@gmail.com>
2022-12-29 01:25:58 +0900

    refactor: getUser 메소드 변경 (세션->토큰)

--- src/main/java/net/sinzak/server/user/controller/UserQueryController.java
+++ src/main/java/net/sinzak/server/user/controller/UserQueryController.java
@@ -4,12 +4,15 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import net.sinzak.server.config.auth.LoginUser;
 import net.sinzak.server.config.auth.dto.SessionUser;
+import net.sinzak.server.user.domain.User;
 import net.sinzak.server.user.dto.respond.GetFollowDto;
+import net.sinzak.server.user.dto.respond.UserDto;
 import net.sinzak.server.user.service.UserQueryService;
 import org.json.simple.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,9 +27,9 @@ public class UserQueryController {
 
     @ApiOperation(value ="유저 프로필 보기")
     @GetMapping(value ="/users/{userId}/")
-    public JSONObject getUser( @PathVariable("userId") Long userId,@ApiIgnore @LoginUser SessionUser user){
-        JSONObject jsonObject = userQueryService.getUserProfile(userId, user);
-        return jsonObject;
+    public UserDto getUser( @PathVariable("userId") Long userId,@ApiIgnore @AuthenticationPrincipal User user){
+        UserDto userDto = userQueryService.getUserProfile(userId, user);
+        return userDto;
     }
     @ApiOperation(value ="팔로워리스트")
     @GetMapping(value ="/users/{userId}/followers")
