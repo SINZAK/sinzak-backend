2f3131c0ece66405e4e31400099063494e17ac93
starwook <star.wookk@gmail.com>
2023-01-12 05:37:00 +0900

    feat: Stomp테스트 컨트롤러 기능 추가

--- src/main/java/net/sinzak/server/chatroom/controller/ChatRoomController.java
+++ src/main/java/net/sinzak/server/chatroom/controller/ChatRoomController.java
@@ -2,20 +2,76 @@ package net.sinzak.server.chatroom.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.sinzak.server.chatroom.domain.ChatMessage;
+import net.sinzak.server.chatroom.domain.ChatRoom;
+import net.sinzak.server.chatroom.dto.ChatMessageDto;
 import net.sinzak.server.chatroom.repository.ChatRoomRepository;
 import net.sinzak.server.chatroom.service.ChatRoomQueryService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
+@Slf4j
+@RequestMapping(value ="/chat")
 public class ChatRoomController {
     private final ChatRoomRepository chatRoomRepository;
     private final ChatRoomQueryService chatRoomQueryService;
+    private final SimpMessagingTemplate template;
 
-//    @Transactional
-//    public JSONObject createChatRoom(){
-//        ChatRoom chatRoom = new ChatRoom();
-//    }
+//    }// /pub/chat/enter로 발행요청을 하면 /sub/chatroom/{roomId}로 메시지 전송
+    @MessageMapping(value = "/enter")
+    public void enter(ChatMessageDto chatMessageDto) {
+        ChatMessage chatMessage = ChatMessage.builder()
+                .message(chatMessageDto.getSender()+"님이 채팅방에 참여하였습니다.")
+                .sender(chatMessageDto.getSender())
+                .roomId(chatMessageDto.getRoomId())
+                .type(chatMessageDto.getMessageType())
+                .build();
+
+        template.convertAndSend("api/sub/chat/room/"+chatMessageDto.getRoomId(),chatMessageDto);
+    }
+    @MessageMapping(value ="/message")
+    public void message(ChatMessageDto chatMessageDto){
+        template.convertAndSend("api/sub/chat/room/"+chatMessageDto.getRoomId(),chatMessageDto);
+    }
+
+    //아래는 테스트용
+    //채팅방 전체조회
+    @GetMapping(value = "/rooms")
+    public ModelAndView getRooms(){
+        log.info("#모든 채팅방 목록");
+        ModelAndView mv = new ModelAndView("chat/rooms");
+        mv.addObject("list",chatRoomRepository.findAll());
+        return mv;
+    }
+    //채팅방 개설
+    @PostMapping(value = "/room")
+    public String createRoom(@RequestParam String roomName, RedirectAttributes rttr){
+        log.info("#채팅방 개설,채팅방 이름: "+ roomName);
+        ChatRoom chatRoom = new ChatRoom(roomName);
+        chatRoomRepository.save(chatRoom);
+        rttr.addFlashAttribute("roomName",chatRoom.getName());
+        return "redirect:/chat/rooms";
+    }
+
+    //채팅방 조회
+    @GetMapping("/room")
+    public void getRoom(String roomId, Model model){
+        log.info("#채팅방 조회, 채팅방 아이디 :" +roomId );
+        model.addAttribute("room",chatRoomRepository.findByRoomId(roomId).get());
+    }
+
+
+
+
+
 
 
 
--- src/main/java/net/sinzak/server/chatroom/service/ChatRoomCommandService.java
+++ src/main/java/net/sinzak/server/chatroom/service/ChatRoomCommandService.java
@@ -24,37 +24,37 @@ public class ChatRoomCommandService {
     private final ChatRoomRepository chatRoomRepository;
     private final UserChatRoomRepository userChatRoomRepository;
 
-    @Transactional
-    public JSONObject createUserChatRoom(User user,User inviteUser){
-        UUID uuid = findIfChatRoomExist(user,inviteUser);
-        if(uuid!=null){
-            return makeChatRoomJson(uuid,inviteUser.getName());
-        }
-        ChatRoom chatRoom = makeChatRoom(user, inviteUser);
-        return makeChatRoomJson(chatRoom.getUuid(),inviteUser.getName());
-    }
-    private JSONObject makeChatRoomJson(UUID uuid,String roomName) {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("success",true);
-        jsonObject.put("roomName",roomName);
-        jsonObject.put("UUID",uuid);
-        return jsonObject;
-    }
-    private UUID findIfChatRoomExist(User user, User inviteUser) {
-        List<UserChatRoom> myUserChatRoom =
-                userChatRoomRepository.findUserChatRoomBySessionUserEmail(user.getEmail());
-        for(UserChatRoom userChatRoom : myUserChatRoom){
-            if(userChatRoom.getRoomName().equals(inviteUser.getName())){
-                return userChatRoom.getChatRoom().getUuid();
-            }
-        }
-        return null;
-    }
-    private ChatRoom makeChatRoom(User user, User inviteUser) {
-        ChatRoom chatRoom = new ChatRoom();
-        chatRoom.addUserChatRoom(user, inviteUser);
-        chatRoom.addUserChatRoom(inviteUser, user);
-        chatRoomRepository.save(chatRoom);
-        return chatRoom;
-    }
+////    @Transactional
+////    public JSONObject createUserChatRoom(User user,User inviteUser){
+////        UUID uuid = findIfChatRoomExist(user,inviteUser);
+////        if(uuid!=null){
+////            return makeChatRoomJson(uuid,inviteUser.getName());
+////        }
+////        ChatRoom chatRoom = makeChatRoom(user, inviteUser);
+////        return makeChatRoomJson(chatRoom.getUuid(),inviteUser.getName());
+////    }
+//    private JSONObject makeChatRoomJson(UUID uuid,String roomName) {
+//        JSONObject jsonObject = new JSONObject();
+//        jsonObject.put("success",true);
+//        jsonObject.put("roomName",roomName);
+//        jsonObject.put("UUID",uuid);
+//        return jsonObject;
+//    }
+//    private UUID findIfChatRoomExist(User user, User inviteUser) {
+//        List<UserChatRoom> myUserChatRoom =
+//                userChatRoomRepository.findUserChatRoomBySessionUserEmail(user.getEmail());
+//        for(UserChatRoom userChatRoom : myUserChatRoom){
+//            if(userChatRoom.getRoomName().equals(inviteUser.getName())){
+//                return userChatRoom.getChatRoom().getUuid();
+//            }
+//        }
+//        return null;
+//    }
+//    private ChatRoom makeChatRoom(User user, User inviteUser) {
+//        ChatRoom chatRoom = new ChatRoom();
+//        chatRoom.addUserChatRoom(user, inviteUser);
+//        chatRoom.addUserChatRoom(inviteUser, user);
+//        chatRoomRepository.save(chatRoom);
+//        return chatRoom;
+//    }
 }
