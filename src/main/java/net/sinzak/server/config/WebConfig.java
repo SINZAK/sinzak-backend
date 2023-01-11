744e92d5bcd24d8e0b4af1c0211d007016835ba7
in-seo <sis000512@gmail.com>
2023-01-11 17:53:28 +0900

    fix: CORS

--- src/main/java/net/sinzak/server/config/WebConfig.java
+++ src/main/java/net/sinzak/server/config/WebConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import net.sinzak.server.config.auth.LoginUserArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,5 +18,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(loginUserArgumentResolver);
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowCredentials(false)
+                .maxAge(3000);
     }
 }
