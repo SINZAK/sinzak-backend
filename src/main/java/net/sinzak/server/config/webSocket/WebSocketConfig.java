c8e7320022097211d2ce047178287ee4caf8c90d
starwook <star.wookk@gmail.com>
2023-01-12 05:34:56 +0900

    refactor: 웹소켓->Stomp

--- src/main/java/net/sinzak/server/config/webSocket/WebSocketConfig.java
+++ src/main/java/net/sinzak/server/config/webSocket/WebSocketConfig.java
@@ -2,26 +2,43 @@ package net.sinzak.server.config.webSocket;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry){
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
-                .addHandler(mySocketHandler(),"/room")
-                .setAllowedOrigins("*");
-    }
-    @Bean
-    public WebSocketHandler mySocketHandler(){
-        return new WebSocketHandler();
+                .addEndpoint("/api/stomp/chat")
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
 
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub");//구독
+        registry.setApplicationDestinationPrefixes("/pub"); //발행
+    }
 }
+
+
+//    @Override
+//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry){
+//        registry
+//                .addHandler(mySocketHandler(),"/room")
+//                .setAllowedOrigins("*");
+//    }
+//    @Bean
+//    public WebSocketHandler mySocketHandler(){
+//        return new WebSocketHandler();
+//    }
+//
+//}
