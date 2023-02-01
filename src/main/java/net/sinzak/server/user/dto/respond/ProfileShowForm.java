496d80e3bd23fadf0aa0943138d329bf69a09bde
starwook <star.wookk@gmail.com>
2023-02-01 19:47:20 +0900

    refactor: 타유저 프로필 조회 기능 판매작품,의뢰작품 포함하도록 수정

--- src/main/java/net/sinzak/server/user/dto/respond/ProfileShowForm.java
+++ src/main/java/net/sinzak/server/user/dto/respond/ProfileShowForm.java
@@ -4,13 +4,13 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 
 @Getter
-public class MyPageShowForm {
+public class ProfileShowForm {
     @ApiModelProperty(example = "글 ID")
     private Long id;
     @ApiModelProperty(example = "https://sinzakimage.s3.ap-northeast-2.amazonaws.com/7aea0508-4b3b-4b52-a98e-8f699b5b4bc7.jpg")
     private String thumbnail;
 
-    public MyPageShowForm(Long id, String thumbnail) {
+    public ProfileShowForm(Long id, String thumbnail) {
         this.id = id;
         this.thumbnail = thumbnail;
     }
