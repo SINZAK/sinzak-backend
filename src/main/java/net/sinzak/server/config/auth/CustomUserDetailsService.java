220d4281a3adf0d46dd9978869d54aad8af00826
inseo <sis000512@gmail.com>
2023-02-28 21:07:23 +0900

    fix: userId token

--- src/main/java/net/sinzak/server/config/auth/CustomUserDetailsService.java
+++ src/main/java/net/sinzak/server/config/auth/CustomUserDetailsService.java
@@ -15,7 +15,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByEmail(username)
+        return userRepository.findById(Long.valueOf(username))
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
     }
 }
