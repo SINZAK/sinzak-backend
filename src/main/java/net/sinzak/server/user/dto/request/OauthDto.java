f94d54568d13a2ea78c1f3b10fc12de0a05722c7
inseo <sis000512@gmail.com>
2023-03-03 19:08:58 +0900

    fix: apple idToken test

--- src/main/java/net/sinzak/server/user/dto/request/OauthDto.java
+++ src/main/java/net/sinzak/server/user/dto/request/OauthDto.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 public class OauthDto {
     @ApiModelProperty(example = "액세스 토큰")
     private String accessToken;
-    @ApiModelProperty(example = "구글전용")
+    @ApiModelProperty(example = "구글, 애플전용")
     private String idToken;
     @ApiModelProperty(example = "소셜 로그인 origin 소문자")
     private String origin;
