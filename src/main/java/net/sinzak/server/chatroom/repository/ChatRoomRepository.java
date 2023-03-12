fe4e56f4a3c4e6f245690aef0e804d2254e32b7d
starwook <star.wookk@gmail.com>
2023-03-13 05:31:31 +0900

    refactor: 게시글에 딸린 상품 로직 수정

--- src/main/java/net/sinzak/server/chatroom/repository/ChatRoomRepository.java
+++ src/main/java/net/sinzak/server/chatroom/repository/ChatRoomRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -13,6 +14,12 @@ import java.util.Optional;
 public interface ChatRoomRepository extends JpaRepository<ChatRoom,Long> {
     @Query("select c from ChatRoom c where c.roomUuid = :roomUuId")
     Optional<ChatRoom> findByRoomId(@Param("roomUuId") String roomUuId);
+
+    @Query("select c from ChatRoom c where c.product.id = :productId")
+    List<ChatRoom> findChatRoomByProductId(@Param("productId") Long productId);
+
+    @Query("select c from ChatRoom c where c.work.id = :workId")
+    List<ChatRoom> findChatRoomByWorkId(@Param("workId") Long workId);
 
     @Query("select c from ChatRoom c left join fetch c.chatMessages where c.roomUuid = :roomUuid")
     Optional<ChatRoom> findByRoomUuidFetchChatMessage(@Param("roomUuid") String roomUuid);
