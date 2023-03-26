51d7b192f25bdb02feb66b564fd0a426def89e8f
inseo <sis000512@gmail.com>
2023-03-27 00:01:27 +0900

    fix: cors handler (local)(2)

--- src/main/java/net/sinzak/server/config/WebConfig.java
+++ src/main/java/net/sinzak/server/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowCredentials(false)
                 .maxAge(3000);
