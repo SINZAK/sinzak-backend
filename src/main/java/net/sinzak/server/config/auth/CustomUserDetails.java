110015ed743abeea49de86be4be4bdb7d9c205a9
inseo <sis000512@gmail.com>
2023-06-15 15:07:34 +0900

    fix: Caching banner, userDetail(2)

--- src/main/java/net/sinzak/server/config/auth/CustomUserDetails.java
+++ src/main/java/net/sinzak/server/config/auth/CustomUserDetails.java
@@ -17,6 +17,9 @@ public class CustomUserDetails implements UserDetails, Serializable {
     private String username;
     private Collection<? extends GrantedAuthority> authorities = null;
     /** authorities는 역직렬화가 안됨. 굳이 하려면 엄청 복잡하게 해야함. 따라서 레디스에 저장은 null로 하고, 불러온 뒤에 권한 세팅 하는 방법으로 고정. **/
+    public String getRole() {
+        return role;
+    }
 
     public void setAuthorities() {
         List<SimpleGrantedAuthority> list = new ArrayList<>();
